8b8e8fb4f82e829a7b12f882914bb0ebc6004f8b
Unknown <fabianrk@gmail.com>
2018-01-30 15:57:26 +0200

    cont

--- includes/js/get_data_main.js
+++ includes/js/get_data_main.js
@@ -1,6 +1,13 @@
-var userName, userPic;
 
-var json= $.getJSON("data.json");
-
-$(document).ready(function(){
-    
+$.getJSON("/includes/js/data.json", function(json) {
+    console.log(json);
+    var user=json.User[0];
+    console.log(user.image);
+    console.log(user.name);
+    var name= JSON.stringify(user.name);
+    console.log(name);
+    var pic= JSON.stringify(user.image);
+    console.log(pic);
+    $("#username").text(name);
+    document.getElementById("#username").innerHTML(name);
+});
--- server/data/db.php
+++ server/data/db.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', '182.50.133.55');
 define('DBUSER', 'auxstudDB7c');
-define('DBPASS', 'auxstud7cDB1');
+define('DBPASS', 'auxstud7cDB1!');
 define('DBNAME', 'auxstudB7c');
 
 
