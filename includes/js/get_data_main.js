1fd9cd01f04b8b473d9d8a2cf6321090b38e03b6
Unknown <fabianrk@gmail.com>
2018-02-01 18:56:40 +0200

    cont2

--- includes/js/get_data_main.js
+++ includes/js/get_data_main.js
@@ -1,13 +1,9 @@
 
-$.getJSON("/includes/js/data.json", function(json) {
+$.getJSON("includes/js/data.json", function(json) {
     console.log(json);
     var user=json.User[0];
     console.log(user.image);
     console.log(user.name);
-    var name= JSON.stringify(user.name);
-    console.log(name);
-    var pic= JSON.stringify(user.image);
-    console.log(pic);
-    $("#username").text(name);
-    document.getElementById("#username").innerHTML(name);
+    $("#username").text(user.name);
+    $(".profile").css("background-image","url("+user.image"+)");
 });
