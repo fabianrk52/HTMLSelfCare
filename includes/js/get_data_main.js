baa52ed6beb8dfa6885587a2e0688a13ea0bb364
Unknown <fabianrk@gmail.com>
2018-02-02 21:23:44 +0200

    main dynamic

--- includes/js/get_data_main.js
+++ includes/js/get_data_main.js
@@ -2,8 +2,35 @@
 $.getJSON("includes/js/data.json", function(json) {
     console.log(json);
     var user=json.User[0];
+    var medicine=json.Medication[0];
+    var hr=json.Watch[0];
+    var app=json.Appointments[0];
+    var msg=json.Messages[0];
+    var tst=json.Test[0];
+
     console.log(user.image);
     console.log(user.name);
+    console.log(medicine.class);
+    console.log(medicine.name);
+    console.log(hr.name);
+    
+
     $("#username").text(user.name);
     $("#profilepic").attr("src",user.image);
+    
+    $("#logomedicine").addClass(medicine.class);
+    $("#namemedicine").text(medicine.name);
+    
+    $("#hrname").text(hr.name);
+    $("#hrimage").attr("src",hr.image);
+    
+    $("#appname").text(app.name);
+    $("#appclass").addClass(app.class);
+
+    $("#msgname").text(msg.name);
+    $("#msgclass").addClass(msg.class);
+    
+    $("#tstname").text(tst.name);
+    $("#tstclass").addClass(tst.class);
+
 });
