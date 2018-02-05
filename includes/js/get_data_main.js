5dabd283a82e27e543893e95afcfeb3c0d784d72
Unknown <fabianrk@gmail.com>
2018-02-05 15:15:37 +0200

    json v2

--- includes/js/get_data_main.js
+++ includes/js/get_data_main.js
@@ -1,36 +1,24 @@
 
 $.getJSON("includes/js/data.json", function(json) {
-    console.log(json);
-    var user=json.User[0];
-    var medicine=json.Medication[0];
-    var hr=json.Watch[0];
-    var app=json.Appointments[0];
-    var msg=json.Messages[0];
-    var tst=json.Test[0];
+    var user= json.User[0];
 
-    console.log(user.image);
-    console.log(user.name);
-    console.log(medicine.class);
-    console.log(medicine.name);
-    console.log(hr.name);
+    $("#username").text(user.Title.name);
+    $("#profilepic").attr("src",user.Title.image);
     
 
-    $("#username").text(user.name);
-    $("#profilepic").attr("src",user.image);
-    
-    $("#logomedicine").addClass(medicine.class);
-    $("#namemedicine").text(medicine.name);
-    
-    $("#hrname").text(hr.name);
-    $("#hrimage").attr("src",hr.image);
-    
-    $("#appname").text(app.name);
-    $("#appclass").addClass(app.class);
+    $("#namemedicine").text(user.Medication.name);
+    $("#logomedicine").addClass(user.Medication.class);
 
-    $("#msgname").text(msg.name);
-    $("#msgclass").addClass(msg.class);
     
-    $("#tstname").text(tst.name);
-    $("#tstclass").addClass(tst.class);
+    $("#hrname").text(user.Watch.name);
+    $("#hrimage").attr("src",user.Watch.image);
+    
+    $("#appname").text(user.Appointments.name);
+    $("#appclass").addClass(user.Appointments.class);
 
-});
+    $("#msgname").text(user.Messages.name);
+    $("#msgclass").addClass(user.Messages.class);
+    
+    $("#tstname").text(user.Test.name);
+    $("#tstclass").addClass(user.Test.class);
+    });
